974e9ffe442f97c04831ee47d845080a5d82709b
arongsnuna <unhi000@naver.com>
2023-03-22 16:13:25 +0900

    update

---
+++ 2_JavaScript/js101.js
@@ -0,0 +1,10 @@
+console.log(400)
+console.log("<br>") //띄어쓰기
+console.log("bus")
+console.log("hello")
+
+let str = "Good morning! Have a nice day."
+let a = str.split(' ')
+console.log(typeof(a))
+console.log("<br>")
+console.log(a[0])
